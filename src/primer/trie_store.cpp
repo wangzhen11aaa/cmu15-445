de1861a4ce934c04800aab587239edc0159dcdcb
WangZhen <wangzhenaaa7@gmail.com>
2023-12-08 16:24:50 +0800

    Project0, Task#2

--- src/primer/trie_store.cpp
+++ src/primer/trie_store.cpp
@@ -11,20 +11,52 @@ auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<T>> {
   // (2) Lookup the value in the trie.
   // (3) If the value is found, return a ValueGuard object that holds a reference to the value and the
   //     root. Otherwise, return std::nullopt.
-  throw NotImplementedException("TrieStore::Get is not implemented.");
+  // throw NotImplementedException("TrieStore::Get is not implemented.");
+
+  // Get Lock to fetch one stable refernece to one shared_ptr<T> Trie.
+  root_lock_.lock();
+  Trie trie = root_;
+  root_lock_.unlock();
+
+  // If not found
+  const T *value = trie.Get<T>(key);
+  if (!value) {
+    return std::nullopt;
+  } else {
+    // Create ValueGuard
+    return ValueGuard<T>(trie, *value);
+  }
 }
 
 template <class T>
 void TrieStore::Put(std::string_view key, T value) {
   // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
   // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Put is not implemented.");
+  // throw NotImplementedException("TrieStore::Put is not implemented.");
+  write_lock_.lock();
+  root_lock_.lock();
+  Trie trie = root_;
+
+  root_ = trie.Put<T>(key, std::move(value));
+
+  root_lock_.unlock();
+  write_lock_.unlock();
 }
 
 void TrieStore::Remove(std::string_view key) {
   // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
   // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Remove is not implemented.");
+  // throw NotImplementedException("TrieStore::Remove is not implemented.");
+
+  write_lock_.lock();
+
+  root_lock_.lock();
+
+  Trie trie = root_;
+  root_ = trie.Remove(std::move(key));
+
+  root_lock_.unlock();
+  write_lock_.unlock();
 }
 
 // Below are explicit instantiation of template functions.
